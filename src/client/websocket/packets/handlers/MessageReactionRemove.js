7926355e56ad2403a74488b4ef6b638f22468c39
TheBeast0403 <89011564+TheBeast0403@users.noreply.github.com>
2022-07-19 23:49:45 +0100

    Update MessageReactionRemove.js

--- src/client/websocket/packets/handlers/MessageReactionRemove.js
+++ src/client/websocket/packets/handlers/MessageReactionRemove.js
@@ -2,9 +2,11 @@ const AbstractHandler = require('./AbstractHandler');
 
 class MessageReactionRemove extends AbstractHandler {
   handle(packet) {
-    const client = this.packetManager.client;
+	  try {
+		      const client = this.packetManager.client;
     const data = packet.d;
     client.actions.MessageReactionRemove.handle(data);
+	  } catch {}
   }
 }
 
