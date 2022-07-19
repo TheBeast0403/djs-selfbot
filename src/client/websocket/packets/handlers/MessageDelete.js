896549399a1f209509b017717ebb7e56db49c2c4
TheBeast0403 <89011564+TheBeast0403@users.noreply.github.com>
2022-07-19 23:47:32 +0100

    Update MessageDelete.js

--- src/client/websocket/packets/handlers/MessageDelete.js
+++ src/client/websocket/packets/handlers/MessageDelete.js
@@ -3,10 +3,12 @@ const Constants = require('../../../../util/Constants');
 
 class MessageDeleteHandler extends AbstractHandler {
   handle(packet) {
-    const client = this.packetManager.client;
+	  try {
+		      const client = this.packetManager.client;
     const data = packet.d;
     const response = client.actions.MessageDelete.handle(data);
     if (response.message) client.emit(Constants.Events.MESSAGE_DELETE, response.message);
+	  } catch {}
   }
 }
 
