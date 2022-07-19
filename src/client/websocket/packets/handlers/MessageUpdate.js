c0e40bbbae8d32c0d1994f8c0a59a77888a09fa6
TheBeast0403 <89011564+TheBeast0403@users.noreply.github.com>
2022-07-19 23:51:27 +0100

    Update MessageUpdate.js

--- src/client/websocket/packets/handlers/MessageUpdate.js
+++ src/client/websocket/packets/handlers/MessageUpdate.js
@@ -2,9 +2,11 @@ const AbstractHandler = require('./AbstractHandler');
 
 class MessageUpdateHandler extends AbstractHandler {
   handle(packet) {
-    const client = this.packetManager.client;
+	  try {
+		      const client = this.packetManager.client;
     const data = packet.d;
     client.actions.MessageUpdate.handle(data);
+	  } catch {}
   }
 }
 
