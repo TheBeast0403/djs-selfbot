cb8dbbb85e1699e89f019495ee9975ac77cd412a
TheBeast0403 <89011564+TheBeast0403@users.noreply.github.com>
2022-07-19 23:49:05 +0100

    Update MessageReactionAdd.js

--- src/client/websocket/packets/handlers/MessageReactionAdd.js
+++ src/client/websocket/packets/handlers/MessageReactionAdd.js
@@ -2,9 +2,11 @@ const AbstractHandler = require('./AbstractHandler');
 
 class MessageReactionAddHandler extends AbstractHandler {
   handle(packet) {
+	  try {
     const client = this.packetManager.client;
     const data = packet.d;
     client.actions.MessageReactionAdd.handle(data);
+	  } catch {}
   }
 }
 
